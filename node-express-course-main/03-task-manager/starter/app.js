3a418ff5b44eae4bf635a7c67ac1201343738b74
SyfaCollins <coniplexy@gmail.com>
2022-08-23 23:56:30 +0300

    task manager

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+/** @format */
+
+// console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+/**
+ * routes
+ */
+
+app.get("/api", (req, res) => {
+  res.send("Task Manager");
+  res.end();
+});
+
+/**
+ * standard server
+ */
+const port = 3000;
+app.listen(port, console.log(`Server running on port ${port}`));

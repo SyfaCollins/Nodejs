7936ffc05f22c03b6b3d5c30df308d0730265983
SyfaCollins <coniplexy@gmail.com>
2022-08-25 00:55:27 +0300

    task manager

--- node-express-course-main/03-task-manager/starter/controllers/tasks.js
+++ node-express-course-main/03-task-manager/starter/controllers/tasks.js
@@ -3,6 +3,26 @@ const getAllTasks = (req,res) => {
     res.send('This is a controller for all tasks')
 }
 
+const createTask = (req,res) =>{
+    res.json(req.body)
+}
+
+const getTask = (req,res) =>{
+    res.json({id:req.params.id})
+}
+
+const updateTask = (req,res) =>{
+    res.send('update a task')
+}
+
+const deleteTask = (req,res) =>{
+    res.send('delete a task')
+}
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 } 

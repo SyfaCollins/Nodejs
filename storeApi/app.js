f010747ee5f028c77e54bbcbf81047ba6bbe9136
SyfaCollins <coniplexy@gmail.com>
2022-09-11 11:20:35 +0300

    added middleware files

--- storeApi/app.js
+++ storeApi/app.js
@@ -8,21 +8,43 @@ const app = express();
 const connectDB = require("./db/connect");
 
 //middleware
+app.use(express().json);
+
+const errorHandlerMiddleware = require("./middleWare/errorHandlerMiddleware");
+const notFoundMiddleWare = require("./middleWare/notFoundMiddleWare");
 
 //PORT
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/store/api/products', (req,res) => {
-  res.send(`<h1>Product Store</h1>`)
-})
-
-app.listen(port, async () => {
-  try {
-    // await connectDB(process.env.MONGO_URI),
-      console.log("connected to database"),
-      app.get(port, (req,res) => res.json({msg:'Hello world'}));
-  } catch (error) {
-    console.log(error);
-  }
-  //   process.env.MONGO_URI, console.log("connected to database");
+//Routes
+app.get("/store/api/products", (req, res) => {
+  res.send(
+    `<h1>Product Store</h1><a href="/api/v1/products"> view all products</a>`
+  );
 });
+
+app.get("/api/v1/products", (req, res) => {
+  res.send(`<h1>Home Products page</h1>`);
+});
+
+app.use("/api/v1/products");
+app.use(notFoundMiddleWare);
+app.use(errorHandlerMiddleware);
+
+const startApp = () => {
+  app.listen(port, async () => {
+    try {
+      return (
+        // await connectDB(process.env.MONGO_URI),
+        console.log(`connected to database and listening at port ${port}`),
+        app.get(port, (req, res) =>
+          res.status(200).json({ msg: "Hello world" })
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+startApp();

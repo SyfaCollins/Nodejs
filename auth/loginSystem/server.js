f1690845dd8dba892adbf5f6644a0f87e822451a
SyfaCollins <coniplexy@gmail.com>
2022-09-18 12:59:46 +0300

    loginsys

--- auth/loginSystem/server.js
+++ auth/loginSystem/server.js
@@ -9,13 +9,19 @@ app.set("view engine", "ejs");
 //home route
 app.get("/", (req, res) => {
   res.render("index", { title: "Login System" });
-  // res.send("Hello world");
+  res.send("page loaded successful");
 });
 
-app.get("/things", (req, res) => {
+app.get("/login", (req, res) => {
   console.log(req.method)
-  // res.render("base", { title: "Login System" });
-  // res.send("Hello world");
+  res.render("login", { title: "Login System" });
+  res.send("page loaded successful");
+});
+
+app.get("/register", (req, res) => {
+  console.log(req.method)
+  res.render("register", { title: "Login System" });
+  res.send("page loaded successful");
 });
 
 app.listen(port, () => {

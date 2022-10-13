0f2c2295f418ae98ceb8e0df07acd0b5b4e3f2b8
SyfaCollins <coniplexy@gmail.com>
2022-10-13 12:34:44 +0300

    login system

--- auth/loginSystem/routes/userAuth.js
+++ auth/loginSystem/routes/userAuth.js
@@ -14,15 +14,15 @@ const { registerUser } = require("../controller/registerUser");
 
 router.route("/").get(registerHome);
 router.route("/login").get(loginUser);
-router.route("/login").post(
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+// router.route("/login").post(
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/login",
+//     failureFlash: true,
+//   })
+// );
 
 router.route("/register").get(register);
-router.route("/register").post(registerUser);
+// router.route("/register").post(registerUser);
 
 module.exports = router;
--- auth/loginSystem/server.js
+++ auth/loginSystem/server.js
@@ -11,40 +11,67 @@ require;
 const userAuth = require("./routes/userAuth");
 const flash = require("express-flash");
 const session = require("express-session");
-const passport = require('passport')
+const passport = require("passport");
+// const {registerUser} = require("./controller/registerUser")
 
-// app.use;
 
-const initializePassport = require("./passport-config");
-initializePassport(passport, (email) => {
-  user.find((user) => user.email === email);
+const users = [];
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //after creating the data redirect users to the login page
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/register");
+  }
+  console.log(users);
 });
 
-const port = process.env.PORT || 3600;
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "ejs");
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) =>
+  users.find((user) => user.email === email)
+);
 
 //userDatabase
 
-const user = [];
+// const user = [];
+
+const port = process.env.PORT || 3600;
 
 //middleWare
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 app.use(flash());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized:false
+    resave: false,
+    saveUninitialized: false,
   })
 );
 
-app.use(passport.initialize())//this is inside the passport
-app.use(passport.session())
-
+app.use(passport.initialize()); //this is inside the passport
+app.use(passport.session());
 
 //home route
-
-app.use(userAuth);
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+app.use(userAuth);``
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);

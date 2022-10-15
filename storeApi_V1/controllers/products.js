9e020c8931c3838ca9dc2ca03f1b6e3f48e730d3
SyfaCollins <coniplexy@gmail.com>
2022-10-16 00:17:29 +0300

    storeApi

--- storeApi_V1/controllers/products.js
+++ storeApi_V1/controllers/products.js
@@ -8,15 +8,27 @@ apply to our request and response
 */
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.findOne({name:"accent chair"});
-  res.status(200).json({products, nbHits: products.length});
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
   // throw new Error("This is testing the async error");
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
-  // console.log(req.query);
-  res.status(200).json({products, nbHits: products.length});
+  const { featured ,company} = req.query;
+
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false; 
+  }
+
+  if (company) {
+    queryObject.company = company === company;
+  }
+
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {

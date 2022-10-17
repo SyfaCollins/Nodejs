c016a80be1ca79b352033cecdf2c8a2120c89ae8
SyfaCollins <coniplexy@gmail.com>
2022-10-17 20:32:08 +0300

    storeApi

--- storeApi_V1/controllers/products.js
+++ storeApi_V1/controllers/products.js
@@ -8,13 +8,17 @@ apply to our request and response
 */
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+
+  const search = 'a'
+  const products = await Product.find({ name: {$regex:search, $options:'i'} });
+
+
   res.status(200).json({ products, nbHits: products.length });
   // throw new Error("This is testing the async error");
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured ,company} = req.query;
+  const { featured ,company, name} = req.query;
 
   const queryObject = {};
 
@@ -23,8 +27,16 @@ const getAllProducts = async (req, res) => {
   }
 
   if (company) {
-    queryObject.company = company === company;
+    queryObject.company = company;
   }
+
+  if(name) {
+    queryObject.name = name
+  }
+
+  // if(sort){
+
+  // }
 
   console.log(queryObject);
   const products = await Product.find(queryObject);
--- storeApi_V1/models/productModel.js
+++ storeApi_V1/models/productModel.js
@@ -1,34 +1,41 @@
-//This file holds the structure of the data to be collected 
+/** @format */
 
-const mongoose = require('mongoose');
+//This file holds the structure of the data to be collected
+
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        require : [true, 'Name is required']
-    },
+  name: {
+    type: String,
+    require: [true, "Name is required"],
+  },
 
-    price:{
-        type:Number,
-        required:[true, 'product price must be provided']
-    },
-    featured:{
-        type:Boolean,
-        default:false,
-    },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
 
-    rating:{
-        type:Number,
-        default:4.5,
-    },
-    createAt:{
-        type:Date,
-        default:Date.now(),
-    },
-    enum:{
-        values:['ikea','liddy','caressa', 'marcos'],
-        // message:' is not supported', 
-    }
-})
+  company: {
+    type: String,
+    required: [true, "Select a company"],
+  },
 
-module.exports = mongoose.model('Product',productSchema)
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  enum: {
+    values: ["ikea", "liddy", "caressa", "marcos"],
+    // message:' is not supported',
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);

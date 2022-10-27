78405e6e0da500b26771e40f27f182a1ca36db51
SyfaCollins <coniplexy@gmail.com>
2022-10-27 17:00:21 +0300

    webToken

--- JSON webToken/controllers/main.js
+++ JSON webToken/controllers/main.js
@@ -1,15 +1,32 @@
 /** @format */
 
-const dashboard = async(req, res) => {
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
+const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
 
-  res.status(200).json({msg:`You can Access you private content ${luckyNumber}`})
+  res
+    .status(200)
+    .json({ msg: `You can Access you private content ${luckyNumber}` });
 };
 
 const login = (req, res, next) => {
-  const { name, password } = req.query;
-  console.log(name, password);
-  res.send('logged In')
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "User created", token });
+
+  console.log(username, password);
+  res.send("logged In");
   next();
 };
 

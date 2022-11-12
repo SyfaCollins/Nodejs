c5b640678b5df48f90f94e6404222341229fc03d
SyfaCollins <coniplexy@gmail.com>
2022-11-13 02:04:24 +0300

    jobApi

--- JobsApi/routes/auth.js
+++ JobsApi/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
 
-module.exports = router
+const {registerUser,loginUser} = require('../controllers/auth')
+
+router.get('/register',registerUser)
+router.get('/login',loginUser)

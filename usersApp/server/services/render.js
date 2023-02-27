9405b2e1945a3cbbf7eeb7f15a56a4756584cb7a
SyfaCollins <coniplexy@gmail.com>
2023-02-28 01:34:07 +0300

    frontend

--- usersApp/server/services/render.js
+++ usersApp/server/services/render.js
@@ -1,12 +1,24 @@
-exports.homeRoute = (req,res)=>{
-    res.render('index')
-}
+/** @format */
 
-exports.addUser = (req,res)=>{
-    res.render("add_user");
-}
+const axios = require("axios");
+const { response } = require("express");
 
-exports.updateUser = (req,res)=>{
-    res.render("update_user");
-}
+exports.homeRoute = (req, res) => {
+  axios
+    .get("http://localhost:8080/api/users")
+    .then(function (response) {
+      console.log(response.data);
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
+exports.addUser = (req, res) => {
+  res.render("add_user");
+};
+
+exports.updateUser = (req, res) => {
+  res.render("update_user");
+};
